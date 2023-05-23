1c8e0d2d254d81165f6ae36351f4162fcb5e68e6
yasna2002 <yasnatalebi@gmail.com>
2023-05-23 16:23:27 +0330

    Add files via upload

---
+++ Design.py
@@ -0,0 +1,39 @@
+def colour_vertices(areas):
+    vertices = sorted((list(areas.keys())))
+    colour_graph = {}
+
+    for vertex in vertices:
+        unused_colours = len(vertices) * [True]
+
+        for neighbor in areas[vertex]:
+            if neighbor in colour_graph:
+                colour = colour_graph[neighbor]
+                unused_colours[colour] = False
+        for colour, unused in enumerate(unused_colours):
+            if unused:
+                colour_graph[vertex] = colour
+                break
+
+    return colour_graph
+
+
+if __name__ == '__main__':
+
+    areas = dict()
+    n = int(input("number of areas : "))
+    print("enter areas related to each other with the following format :"
+          " a b c ... \na : main vertex")
+    for i in range(n):
+        vertex_list = input().split()
+        key = vertex_list.pop(0)
+        areas[key] = vertex_list
+
+    #list_of_carpets = []
+    colored_carpet = (colour_vertices(areas))
+    #list_of_carpets.append(colored_carpet)
+    max = 0
+    for i in colored_carpet.values():
+        if i > max:
+            max = i
+    print("Maximum color needed to color the carpet : ", max + 1)
+    print(colored_carpet)
---
+++ Navigation.py
@@ -0,0 +1,87 @@
+
+class Graph:
+
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+
+    def printSolution(self, dist, end, distance_result):
+
+        print(dist[end])
+        print(*distance_result)
+
+    def minDistance(self, dist, sptSet):
+
+        min = 1e7
+
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+
+        return min_index
+
+    def dijkstra(self, start, end, cities):
+
+        dist = [1e7] * self.V
+        dist[start] = 0
+        sptSet = [False] * self.V
+        distance_result = []
+
+        for cout in range(self.V):
+
+            u = self.minDistance(dist, sptSet)
+
+            sptSet[u] = True
+
+            for v in range(self.V):
+                if (self.graph[u][v] > 0 and
+                        sptSet[v] == False and
+                        dist[v] > dist[u] + self.graph[u][v]):
+                    dist[v] = dist[u] + self.graph[u][v]
+            distance_result.append(cities[u])
+            if u == end:
+                break
+
+        self.printSolution(dist, end, distance_result)
+
+
+if __name__ == '__main__':
+
+    number_of_intersections = 15
+    g = Graph(number_of_intersections)
+
+    intersections = ["Vokala", "Freiburg", "Azadi", "Enghelab", "Shariati", "Taleghani", "Afarinesh",
+                     "Valiasr", "Emam","Jomhori","Shohada","Takhti","Daneshgah","Bahonar","Baharestan"]
+
+    number_of_streets = 24
+
+    streets = [["Vokala","Freiburg","300"],["Vokala","Enghelab","200"],["Vokala","Valiasr","100"],
+               ["Freiburg","Azadi","400"],["Azadi","Shariati","250"],["Freiburg","Shariati","350"],
+               ["Freiburg","Taleghani","280"],["Taleghani","Valiasr","330"],["Valiasr","Emam","360"],
+               ["Valiasr","Jomhori","500"],["Emam","Jomhori","450"],["Emam","Baharestan","900"],
+               ["Jomhori","Baharestan","800"],["Bahonar","Baharestan","700"],["Bahonar","Daneshgah","300"],
+               ["Bahonar","Takhti","400"],["Bahonar","Jomhori","650"],["Takhti","Daneshgah","250"],
+               ["Takhti","Shohada","500"],["Daneshgah","Shohada","200"],["Taleghani","Shohada","950"],
+               ["Afarinesh","Shohada","400"],["Shariati","Afarinesh","150"],["Enghelab","Taleghani","250"]]
+
+    start_end = input().split(" ")
+    start_end = [str(x) for x in start_end]
+
+    g.graph = []
+    for city in intersections:
+        list = []
+        for i in range(number_of_intersections):
+            list.append(0)
+        for i in range(number_of_streets):
+            if city == streets[i][0]:
+                index = intersections.index(streets[i][1])
+                list[index] = int(streets[i][2])
+            elif city == streets[i][1]:
+                index = intersections.index(streets[i][0])
+                list[index] = int(streets[i][2])
+
+        g.graph.append(list)
+
+    g.dijkstra(intersections.index(start_end[0]), intersections.index(start_end[1]), intersections)
