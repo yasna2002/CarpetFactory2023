b29f30c954f2d5df73c45833c44db5bc60f0770f
yasna2002 <yasnatalebi@gmail.com>
2023-05-25 18:45:27 +0330

    quick sort added

--- Search.py
+++ Search.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def matrix_match_percentage(matrix1, matrix2):
+def carpet_match_percentage(matrix1, matrix2):
     # Convert the matrices to numpy arrays for easier comparison
     arr1 = np.array(matrix1)
     arr2 = np.array(matrix2)
@@ -16,3 +16,14 @@ def matrix_match_percentage(matrix1, matrix2):
     percentage_match = (matching_elements / total_elements) * 100
 
     return percentage_match
+
+
+def quicksort_high_to_low(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x > pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x < pivot]
+    return quicksort_high_to_low(left) + middle + quicksort_high_to_low(right)
+
